2c09c4fb1245997e0ca967660f3b3e86c1c69c79
jun931223 <jun931223@gmail.com>
2022-11-30 06:09:22 +0900

    feat: add css loader to templates of jsx, tsx

--- templates/front_react_jsx/webpack.config.js
+++ templates/front_react_jsx/webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
           options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
--- templates/front_react_tsx/webpack.config.js
+++ templates/front_react_tsx/webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'ts-loader',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
